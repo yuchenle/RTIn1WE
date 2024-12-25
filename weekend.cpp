a95224fe26ff7786997a9abd2c33d1c2389fd0cf
Chenle <ycle@live.fr>
2024-12-25 17:11:07 +0100

    adding tracking mechanism

--- weekend.cpp
+++ weekend.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 int main() {
 
@@ -12,6 +13,8 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
+        std::clog << std::setprecision(3) << ((double)j)/image_height*100 << " %" << std::flush;
+        
         for (int i = 0; i < image_width; i++) {
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
@@ -23,5 +26,6 @@ int main() {
 
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
+        std::clog << "Done\n" ;
     }
 }
